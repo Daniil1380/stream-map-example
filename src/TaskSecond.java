f50658533d1ba2b2d94b738f63c09f6c50a1f291
datky <tkachenko.d@cdek.ru>
2024-07-02 20:41:19 +0300

    commit

--- src/TaskSecond.java
+++ src/TaskSecond.java
@@ -1,6 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskSecond {
 
     public static void main(String[] args) {
+
+        //есть список родителей, отдайте список детей
+
+        Person ivan = new Person("Иван");
+        Person sergey = new Person("Сергей");
+        List<Person> arturChildren = new ArrayList<>();
+        arturChildren.add(ivan);
+        arturChildren.add(sergey);
+        Person artur = new Person("Артур", arturChildren);
+
+
+        Person daniil = new Person("Даниил");
+        List<Person> vladimirChildren = new ArrayList<>();
+        vladimirChildren.add(daniil);
+        Person vladimir = new Person("Владимир", vladimirChildren);
+
+
+        Person anna = new Person("Анна");
+        List<Person> alexanderChildren = new ArrayList<>();
+        alexanderChildren.add(anna);
+        Person alexander = new Person("Александр", alexanderChildren);
+
+
+        Person anton = new Person("Антон");
+
+
+
+
 
     }
 }
